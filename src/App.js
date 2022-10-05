e486d294254ce51582f02596cb352dcc0c45dbaa
suelytohm <suelytohm.02@gmail.com>
2022-10-04 22:47:05 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { FormProdutos } from "./components/FormProdutos";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Teste</h1>
+      <FormProdutos />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FormProdutos/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./FormProdutos.css"
+export const FormProdutos = () => {
+    return (
+        <div className="form-produtos">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa eum ipsa, doloremque corporis officiis iste perferendis, id nulla laborum ipsam quas voluptates, dignissimos itaque nemo deserunt perspiciatis. Aliquid, aperiam ab!
+            </p>
+        </div>
+    )
+}
